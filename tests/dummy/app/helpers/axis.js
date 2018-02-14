b02c49213a732fa7b869534fd5c76b5988a1b24b
Gareth Bosch <gbosch@gmail.com>
2018-02-13 22:11:59 -0900

    added new helper to create chart axis elements

---
+++ tests/dummy/app/helpers/axis.js
@@ -0,0 +1,11 @@
+import { helper } from '@ember/component/helper';
+
+export function axis(param/*, hash*/) {
+
+  let str = '<line x1="10%" x2="10%" y1="" y2="10%" stroke="grey" stroke-width="1"/>';
+  let line = document.createElement("line");
+
+  return line;
+}
+
+export default helper(axis);
--- tests/dummy/app/helpers/scale-y.js
+++
@@ -1,7 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export function scaleY([value], {minValue, maxValue}) {
-  return `${100 - (100 * (value - minValue)/(maxValue - minValue))}%`;
-}
-
-export default helper(scaleY);
