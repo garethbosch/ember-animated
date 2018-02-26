9e16d16917df2eccb70bd4491a1d239fc4904d52
Gareth Bosch <gbosch@gmail.com>
2018-02-25 22:39:04 -0900

    Added a function in component to make tick mark positions

--- tests/dummy/app/components/chart-axis.js
+++ tests/dummy/app/components/chart-axis.js
@@ -1,7 +1,6 @@
 import Component from '@ember/component';
 import layout from '../templates/components/chart-axis';
 import { computed } from '@ember/object';
-import { task } from 'ember-concurrency';
 
 
 export default Component.extend({
@@ -9,15 +8,20 @@ export default Component.extend({
   tagName: '',
   tickCount: 10,
   key: '',
-  tickMarks: computed('model', function() {
-    let bounds = findMinMax(this.get('model'), this.get('key'))
+  tickValues: computed('model', function() {
+    let bounds = keyValueBounds(this.get('model'), this.get('key'))
     return bounds;
   }),
+  tickMarks: computed('tickCount', function() {
+    return positionsFor(this.get('tickCount'));
+  }),
 
-  label: 'This might be an axis',
+  label: 'Your Axis',
 });
 
-function findMinMax(rows, key) {
+// Find the minimun and maximun values that exist in all rows of
+//   the model for the given key.
+function keyValueBounds(rows, key) {
   let keyVals = [];
   rows.forEach(row => {
     keyVals.push(row[key]);
@@ -29,4 +33,15 @@ function findMinMax(rows, key) {
     return Math.min(a, b);
   });
   return [lowerBound, upperBound];
+}
+// Find the positions for the tick marks on the axis.
+// Each position is a percentage of the total axis length.
+function positionsFor(numberOfTicks) {
+  let positions = [];
+  let interval = 100/numberOfTicks;
+  for (let i = 0; i+interval <= 100; i+= interval) {
+    positions.push((Math.trunc( (i + interval) * 100 ) 
+      / 100).toString(10) + '%');  // string: percent to 2 dec places
+  }
+  return positions;
 }
