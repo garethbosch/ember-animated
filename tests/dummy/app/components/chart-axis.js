84760c89e6f396c385220e75a6e45f3f87cc9f8e
Gareth Bosch <gbosch@gmail.com>
2018-02-28 22:34:13 -0900

    chart-axis template now displays log scale values

--- tests/dummy/app/components/chart-axis.js
+++ tests/dummy/app/components/chart-axis.js
@@ -35,18 +35,19 @@ function keyValueBounds(rows, key) {
   return [lowerBound, upperBound];
 }
 // Find the positions (in percent) of the tick marks on the axis
-//   and the real values of those positions.
+//   and the real (log2 scale) values of those positions.
 // Returns an array of objects. Each object is a tick mark on the axis
 //   with parameters 'figure' and 'position.'
 function tickPositions(numberOfTicks, bounds) {
   let marks = [];
-  let span = bounds[1] - bounds[0]; // the span of values the axis covers
+  let logMin = Math.log2(bounds[0]);
+  let logMax = Math.log2(bounds[1]);
   let interval = 100/numberOfTicks;
   // count to each interval from 0-100
   for (let i = 0; i+interval <= 100; i+= interval) { 
     let relativePos = (Math.trunc( (i + interval) * 100 ) / 100);// percent to 2 dec places
-    let value = span * (relativePos/100)  // real value represented by the tick mark
-    marks.push({figure: value, position: relativePos.toString(10)+'%'})
+    let value = Math.pow(2, (relativePos/100 * (logMax - logMin) + logMin))  // real value on the log scale
+    marks.push({figure: Math.round(value), position: relativePos.toString(10)+'%'})
   }
   return marks;
 }
