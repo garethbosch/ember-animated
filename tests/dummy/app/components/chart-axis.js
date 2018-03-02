62f324dca10b02cd17b2372ff6d63d02feaa02d9
Gareth Bosch <gbosch@gmail.com>
2018-03-01 22:55:21 -0900

    checks for log scaling when drawing axis

--- tests/dummy/app/components/chart-axis.js
+++ tests/dummy/app/components/chart-axis.js
@@ -6,17 +6,25 @@ import { computed } from '@ember/object';
 export default Component.extend({
   layout,
   tagName: '',
+
   tickCount: 10,
   key: '',
-  tickValues: computed('model', function() {
-    let bounds = keyValueBounds(this.get('model'), this.get('key'))
-    return bounds;
-  }),
-  tickMarks: computed('tickCount', 'tickValues', function() {
-    return tickPositions(this.get('tickCount'), this.get('tickValues'));
-  }),
-
+  log: '2',
+  axis: 'x',
   label: 'Your Axis',
+
+  isXaxis: computed('axis', function() {
+    return this.get('axis') === 'x' || this.get('axis') === 'X';
+  }),
+  isYaxis: computed('axis', function() {
+    return this.get('axis') === 'y' || this.get('axis') === 'Y';
+  }),
+  axisBounds: computed('model', 'key', function() {
+    return keyValueBounds(this.get('model'), this.get('key'));
+  }),
+  tickMarks: computed('tickCount', 'axisBounds', 'log', function() {
+    return tickPositions(this.get('tickCount'), this.get('axisBounds'), this.get('log'), this.get('axis'));
+  }),
 });
 
 // Find the minimun and maximun values that exist in all rows of
@@ -38,15 +46,23 @@ function keyValueBounds(rows, key) {
 //   and the real (log2 scale) values of those positions.
 // Returns an array of objects. Each object is a tick mark on the axis
 //   with parameters 'figure' and 'position.'
-function tickPositions(numberOfTicks, bounds) {
+function tickPositions(numberOfTicks, bounds, log, axis) {
+  let min = bounds[0];
+  let max = bounds[1];
+  if (log === '2') {
+    min = Math.log2(bounds[0]);
+    max = Math.log2(bounds[1]);
+  }
+  if (log === '10') {
+    min = Math.log10(bounds[0]);
+    max = Math.log10(bounds[1]);
+  }  
   let marks = [];
-  let logMin = Math.log2(bounds[0]);
-  let logMax = Math.log2(bounds[1]);
   let interval = 100/numberOfTicks;
   // count to each interval from 0-100
   for (let i = 0; i+interval <= 100; i+= interval) { 
     let relativePos = (Math.trunc( (i + interval) * 100 ) / 100);// percent to 2 dec places
-    let value = Math.pow(2, (relativePos/100 * (logMax - logMin) + logMin))  // real value on the log scale
+    let value = Math.pow(2, (relativePos/100 * (max - min) + min))  // real value on the log scale
     marks.push({figure: Math.round(value), position: relativePos.toString(10)+'%'})
   }
   return marks;
