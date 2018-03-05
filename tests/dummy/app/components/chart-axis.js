d54151f825f5402d272cef673f80ac662c733783
Gareth Bosch <gbosch@gmail.com>
2018-03-04 22:57:47 -0900

    chart-axis component can now draw x- or y-axis using log scale or not.

--- tests/dummy/app/components/chart-axis.js
+++ tests/dummy/app/components/chart-axis.js
@@ -7,11 +7,11 @@ export default Component.extend({
   layout,
   tagName: '',
 
-  tickCount: 10,
-  key: '',
-  log: '2',
-  axis: 'x',
-  label: 'Your Axis',
+  key: '',            // aspect of the model to be used for the axis
+  tickCount: 10,      // how many tick marks displayed on the axis
+  scale: '',          // 'log' or nothing
+  axis: 'x',          // 'x' or 'y' axis
+  label: 'Your Axis', // name of the axis
 
   isXaxis: computed('axis', function() {
     return this.get('axis') === 'x' || this.get('axis') === 'X';
@@ -22,8 +22,8 @@ export default Component.extend({
   axisBounds: computed('model', 'key', function() {
     return keyValueBounds(this.get('model'), this.get('key'));
   }),
-  tickMarks: computed('tickCount', 'axisBounds', 'log', function() {
-    return tickPositions(this.get('tickCount'), this.get('axisBounds'), this.get('log'), this.get('axis'));
+  tickMarks: computed('tickCount', 'axisBounds', 'scale', function() {
+    return tickPositions(this.get('tickCount'), this.get('axisBounds'), this.get('scale'), this.get('isYaxis'));
   }),
 });
 
@@ -42,28 +42,39 @@ function keyValueBounds(rows, key) {
   });
   return [lowerBound, upperBound];
 }
-// Find the positions (in percent) of the tick marks on the axis
-//   and the real (log2 scale) values of those positions.
+// Finds the positions (in percent) of the tick marks on the axis
+//   and the real values of those positions.
 // Returns an array of objects. Each object is a tick mark on the axis
 //   with parameters 'figure' and 'position.'
-function tickPositions(numberOfTicks, bounds, log, axis) {
+function tickPositions(numberOfTicks, bounds, scale, isYaxis) {
   let min = bounds[0];
   let max = bounds[1];
-  if (log === '2') {
-    min = Math.log2(bounds[0]);
-    max = Math.log2(bounds[1]);
-  }
-  if (log === '10') {
-    min = Math.log10(bounds[0]);
-    max = Math.log10(bounds[1]);
-  }  
   let marks = [];
   let interval = 100/numberOfTicks;
-  // count to each interval from 0-100
-  for (let i = 0; i+interval <= 100; i+= interval) { 
-    let relativePos = (Math.trunc( (i + interval) * 100 ) / 100);// percent to 2 dec places
-    let value = Math.pow(2, (relativePos/100 * (max - min) + min))  // real value on the log scale
-    marks.push({figure: Math.round(value), position: relativePos.toString(10)+'%'})
+  let value;
+  let posOnAxis
+  if (scale === 'log') {
+    min = Math.log2(min);
+    max = Math.log2(max);
+      // count to each interval from 0-100
+    for (let i = 0; i+interval <= 100; i+= interval) { 
+      // truncate this percentage of the axis to 2 dec places
+      posOnAxis = (Math.trunc( (i + interval) * 100 ) / 100);
+      // real value of this percentage on a log base 2 scale
+      value = Math.pow(2, (posOnAxis/100 * (max - min) + min));
+      marks.push({figure: Math.round(value), position: posOnAxis.toString(10)+'%'});
+    }
+  } 
+  else if (scale !== 'log') {
+    for (let i = 0; i+interval <= 100; i+= interval) { 
+      posOnAxis = (Math.trunc( (i + interval) * 100 ) / 100);
+      if (isYaxis) {  // origin of svg element is at lop left, so y-axis values must be inverted
+        value = (1 - posOnAxis/100) * (max - min) + min;
+      } else { 
+        value = posOnAxis/100 * (max - min) + min;
+      }
+      marks.push({figure: Math.round(value), position: posOnAxis.toString(10)+'%'})
+    }
   }
   return marks;
 }
