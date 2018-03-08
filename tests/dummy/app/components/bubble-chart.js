4cd1b2d79e6753f3248317c1b007604f0ae0b13b
Gareth Bosch <gbosch@gmail.com>
2018-03-07 18:53:57 -0900

    Applied moveCircle animation to chart bubbles

--- tests/dummy/app/components/bubble-chart.js
+++ tests/dummy/app/components/bubble-chart.js
@@ -2,20 +2,21 @@ import Component from '@ember/component';
 import layout from '../templates/components/bubble-chart';
 import { computed } from '@ember/object';
 import Move from 'ember-animated/motions/move';
-import { task } from 'ember-concurrency';
-import { rAF } from 'ember-animated/concurrency-helpers';
+import { task, timeout } from 'ember-concurrency';
+import { rAF } from 'ember-animated';
+import moveCircle from '../motions/move-circle';
 
 
 export default Component.extend({
 
   currentYear: 1980,
+  fastCurrentYear: 1980,
   points: computed('currentYear', 'model', function(){
     let currentYear = parseInt(this.get('currentYear'));
     let yearlyData = this.get('model').filter(row => row.year === currentYear);
     yearlyData = interpolate(yearlyData);
     return yearlyData.sort((a, b) => b.population - a.population);
   }),
-  transition,
 
   layout,
 
@@ -26,6 +27,12 @@ export default Component.extend({
         yield rAF();
       } 
   }).drop(),
+  updateCurrentYear: task(function * () {
+    while (true) {
+      this.set('currentYear', parseInt(this.get('fastCurrentYear')));
+      yield timeout(200);
+    } 
+}).on('init'),
 
   startingYear: 1950,
   endingYear: 2015,
@@ -39,15 +46,16 @@ export default Component.extend({
     }
   }).drop(),
 
+  moveThem: function * ({ keptSprites }) {
+    keptSprites.forEach(moveCircle);
+  },
+
   actions: { pause() {
     this.get('play').cancelAll();
     this.get('playLoop').cancelAll();
   }},
 });
 
-function * transition() {
-  this.keptSprites.forEach(s => this.animate(new Move(s)));
-}
 function interpolate(objectArray) {
   return objectArray;
 }
